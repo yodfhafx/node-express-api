6ead1a213e1b09740657d3ff8e98b424cb517991
yodfhafx <yodfha2014@gmail.com>
2019-03-05 18:17:34 +0700

    implement tours api

--- build/server.js
+++ build/server.js
@@ -4,12 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const data_1 = require("./data/data");
 const app = express_1.default();
+// console.log(JSON.parse(JSON.stringify(DataStore.tours)));
 app.get('/', (req, res, next) => {
     res.send('Tour Booking API');
 });
 app.get('/tours', (req, res, next) => {
-    res.send('Get a list of tour...');
+    res.json(data_1.DataStore.tours);
 });
 app.post('/tours', (req, res, next) => {
     res.send('Add a new tour...');
--- server.ts
+++ server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import { DataStore } from './data/data';
 const app = express();
+
+// console.log(JSON.parse(JSON.stringify(DataStore.tours)));
 
 app.get('/', (req, res, next) => {
   res.send('Tour Booking API');
 });
 
 app.get('/tours', (req, res, next) => {
-  res.send('Get a list of tour...');
+  res.json(DataStore.tours);
 });
 
 app.post('/tours', (req, res, next) => {
